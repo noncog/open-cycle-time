ce2ba98377db79586218877c6f3db035d1e89b36
Jake Turner <108415765+noncog@users.noreply.github.com>
2023-02-12 16:34:13 -0500

    feat: add base imgui setup (#80)
    Closes #78

--- src/gui_context.cpp
+++ src/gui_context.cpp
@@ -1,21 +1,29 @@
 #include "gui_context.h"
+#include <imgui_impl_glfw.h>
+#include <imgui_impl_opengl3.h>
+#include <implot.h>
 #include <iostream>
 
 namespace oct {
 GUIContext::GUIContext(const std::string& window_title, unsigned short width,
                        unsigned short height)
-    : window{initialiseGLFW(window_title, width, height)} {}
+    : window{initialiseGLFW(window_title, width, height)}
+    , io{GUIContext::initialiseIMGUI()} {}
 GUIContext::~GUIContext() {
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImPlot::DestroyContext();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
     glfwTerminate();
 }
 void GUIContext::loop() {
-    glfwSwapBuffers(window);
     glfwPollEvents();
+    buildGUI();
+    renderGUI();
 }
 bool GUIContext::isActive() const {
-    return glfwGetKey(window, GLFW_KEY_ESCAPE) != GLFW_PRESS
-           && !glfwWindowShouldClose(window);
-    // return !glfwWindowShouldClose(window);
+    return !glfwWindowShouldClose(window);
 }
 GLFWwindow* GUIContext::initialiseGLFW(const std::string& window_title,
                                        unsigned short width,
@@ -28,12 +36,6 @@ GLFWwindow* GUIContext::initialiseGLFW(const std::string& window_title,
     }
 
     // Set GLFW window hints
-    // GLFWmonitor* monitor = glfwGetPrimaryMonitor();
-    // const GLFWvidmode* mode = glfwGetVideoMode(monitor);
-    // glfwWindowHint(GLFW_RED_BITS, mode->redBits);
-    // glfwWindowHint(GLFW_GREEN_BITS, mode->greenBits);
-    // glfwWindowHint(GLFW_BLUE_BITS, mode->blueBits);
-    // glfwWindowHint(GLFW_REFRESH_RATE, mode->refreshRate);
     glfwWindowHint(GLFW_SAMPLES, 2); // TODO: Check anti-aliasing value/need
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
@@ -64,5 +66,115 @@ GLFWwindow* GUIContext::initialiseGLFW(const std::string& window_title,
 }
 void GUIContext::errorCallbackGLFW(int error, const char* description) {
     std::cerr << "GLFW Error (" << error << ") " << description << std::endl;
+}
+ImGuiIO& GUIContext::initialiseIMGUI() {
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+    ImPlot::CreateContext();
+    ImGuiIO& getIo = ImGui::GetIO();
+    (void)getIo;
+    getIo.ConfigFlags |=
+        ImGuiConfigFlags_NavEnableKeyboard; // Enable Keyboard Controls
+
+    // Setup Dear ImGUI style
+    ImGui::StyleColorsDark();
+
+    // Setup Platform/Renderer backends
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init(glsl_version);
+
+    // TODO: Load Fonts
+    // - If no fonts are loaded, dear imgui will use the default font. You can
+    // also load multiple fonts and use ImGui::PushFont()/PopFont() to select
+    // them.
+    // - AddFontFromFileTTF() will return the ImFont* so you can store it if you
+    // need to select the font among multiple.
+    // - If the file cannot be loaded, the function will return NULL. Please
+    // handle those errors in your application (e.g. use an assertion, or
+    // display an error and quit).
+    // - The fonts will be rasterized at a given size (w/ oversampling) and
+    // stored into a texture when calling
+    // ImFontAtlas::Build()/GetTexDataAsXXXX(), which ImGui_ImplXXXX_NewFrame
+    // below will call.
+    // - Use '#define IMGUI_ENABLE_FREETYPE' in your imconfig file to use
+    // Freetype for higher quality font rendering.
+    // - Read 'docs/FONTS.md' for more instructions and details.
+    // - Remember that in C/C++ if you want to include a backslash \ in a string
+    // literal you need to write a double backslash \\ !
+    // io.Fonts->AddFontDefault();
+    // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\segoeui.ttf", 18.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/DroidSans.ttf", 16.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Roboto-Medium.ttf", 16.0f);
+    // io.Fonts->AddFontFromFileTTF("../../misc/fonts/Cousine-Regular.ttf", 15.0f);
+    // ImFont* font =
+    // io.Fonts->AddFontFromFileTTF("c:\\Windows\\Fonts\\ArialUni.ttf", 18.0f,
+    // NULL, io.Fonts->GetGlyphRangesJapanese()); IM_ASSERT(font != NULL);
+
+    return getIo;
+}
+void GUIContext::buildGUI() {
+    // Make a new frame
+    ImGui_ImplOpenGL3_NewFrame();
+    ImGui_ImplGlfw_NewFrame();
+    ImGui::NewFrame();
+
+    // Build the main GUI
+
+    // Debug menu flags
+    static bool show_imgui_demo = false;
+    static bool show_implot_demo = false;
+    static bool show_implot_metrics = false;
+    static bool show_imgui_metrics = false;
+
+    // Debug menu actions
+    if (show_imgui_demo) {
+        ImGui::ShowDemoWindow(&show_imgui_demo);
+    }
+    if (show_implot_demo) {
+        ImPlot::ShowDemoWindow(&show_implot_demo);
+    }
+    if (show_imgui_metrics) {
+        ImGui::ShowMetricsWindow(&show_imgui_metrics);
+    }
+    if (show_implot_metrics) {
+        ImPlot::ShowMetricsWindow(&show_implot_metrics);
+    }
+
+    // Main menu bar
+    if (ImGui::BeginMainMenuBar()) {
+        if (ImGui::BeginMenu("File")) {
+            if (ImGui::MenuItem("Quit", nullptr)) {
+                glfwSetWindowShouldClose(window, 1);
+            }
+            ImGui::EndMenu();
+        }
+        if (ImGui::BeginMenu("Debug")) {
+            if (ImGui::MenuItem("Show ImGUI Demo", nullptr, &show_imgui_demo)) {
+            }
+            if (ImGui::MenuItem("Show ImPlot Demo", nullptr,
+                                &show_implot_demo)) {
+            }
+            if (ImGui::MenuItem("Show ImGUI Metrics", nullptr,
+                                &show_imgui_metrics)) {
+            }
+            if (ImGui::MenuItem("Show ImPlot Metrics", nullptr,
+                                &show_implot_metrics)) {
+            }
+            ImGui::EndMenu();
+        }
+        ImGui::EndMainMenuBar();
+    }
+}
+void GUIContext::renderGUI() {
+    ImGui::Render();
+
+    glfwGetFramebufferSize(window, &display_w, &display_h);
+    glViewport(0, 0, display_w, display_h);
+    glClearColor(clear_color.x * clear_color.w, clear_color.y * clear_color.w,
+                 clear_color.z * clear_color.w, clear_color.w);
+    glClear(GL_COLOR_BUFFER_BIT);
+    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+    glfwSwapBuffers(window);
 }
 } // namespace oct

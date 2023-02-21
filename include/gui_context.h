e0643744790d9c0e5c4543364af61547a32df6eb
Jake Turner <108415765+noncog@users.noreply.github.com>
2023-02-20 23:12:15 -0500

    refactor: move main gui to its own class (#106)

--- include/gui_context.h
+++ include/gui_context.h
@@ -2,10 +2,10 @@
 #ifndef OPEN_CYCLE_TIME_GUI_CONTEXT_H
 #define OPEN_CYCLE_TIME_GUI_CONTEXT_H
 #define GLFW_INCLUDE_NONE
+#include "gui_functions.h"
 #include <GLFW/glfw3.h>
 #include <glad/glad.h>
 #include <imgui.h>
-#include <opencv2/opencv.hpp>
 #include <string>
 #include <vector>
 
@@ -17,29 +17,15 @@ public:
     ~GUIContext();
     void loop();
     bool isActive() const;
+    GLFWwindow* window;
 
 private:
     const char* glsl_version = "#version 410";
-    GLFWwindow* window;
     ImGuiIO& io;
     int display_w;
     int display_h;
     ImVec4 clear_color = ImVec4(0.29f, 0.29f, 0.38f, 0.4f);
-    std::string file_name;
-    bool typeCheck();
-    cv::Mat frame;
-    std::vector<cv::Mat> frames;
-    int frame_rate;
-    int first_frame;
-    int last_frame;
-    int max_frame;
-    float video_length;
-    GLuint texture;
-    bool texture_current;
-    int viewer_width;
-    int viewer_height;
-    int current_frame;
-    int prev_frame;
+    oct::GUIFunctions gui;
     static void errorCallbackGLFW(int error, const char* description);
     static GLFWwindow* initialiseGLFW(const std::string& window_title,
                                       unsigned short width,
